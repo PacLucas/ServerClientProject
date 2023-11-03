dd43d42c00b4ec963ac09ae6bc3b0d3dedc86829
Lucas Pacheco <lucaspacheco@alunos.utfpr.edu.br>
2023-11-03 00:17:13 -0300

    [feature] Melhorias navegacao e finalizacao CRUD Usuario

---
+++ src/main/java/com/cliente/Usuario.java
@@ -0,0 +1,46 @@
+package com.cliente;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Usuario {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
+    private String nome;
+    @JsonProperty("type")
+    private String tipo;
+    @JsonProperty("email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
